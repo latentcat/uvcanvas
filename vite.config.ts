50f28833a88dbbabbb4bf0698001bde4ab91264e
CPunisher <1343316114@qq.com>
2024-03-01 23:25:32 +0800

    Make react external

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "react/jsx-runtime"],
     },
   },
 });
