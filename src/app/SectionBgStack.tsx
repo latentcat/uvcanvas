4cc9d3f1ffc856ea93562cae92e7ca2ad25fb5fd
ciaochaos <1272777550@qq.com>
2024-03-01 15:46:13 +0800

    update

--- src/app/SectionBgStack.tsx
+++ src/app/SectionBgStack.tsx
@@ -4,11 +4,41 @@ import {AspectRatio} from "@/components/ui/aspect-ratio";
 import {clsx} from "clsx";
 import {motion} from "framer-motion"
 import {transitionLg} from "@/lib/animations";
+import {useId} from "react";
+
+
+
+export function Glow() {
+  let id = useId()
+
+  return (
+    <div className="absolute inset-0 -z-10 overflow-hidden _bg-gray-50 _dark:bg-gray-950 dark:bg-black">
+      <svg
+        className="absolute -bottom-48 left-[-40%] h-[80rem] w-[180%]"
+        aria-hidden="true"
+      >
+        <defs>
+          <radialGradient id={`${id}-mobile`} cy="100%">
+            <stop offset="0%" stopColor="rgba(255, 255, 255, 0.3)" />
+            <stop offset="53.95%" stopColor="rgba(255, 255, 255, 0.1)" />
+            <stop offset="100%" stopColor="rgba(255, 255, 255, 0)" />
+          </radialGradient>
+        </defs>
+        <rect
+          width="100%"
+          height="100%"
+          fill={`url(#${id}-mobile)`}
+        />
+      </svg>
+      <div className="absolute inset-x-0 bottom-0 right-0 h-px bg-white mix-blend-overlay" />
+    </div>
+  )
+}
 
 const genParam = (index: number) => {
   let start = 10
   let k = (1 / (start + index)) * start
-  let offsetY = 700
+  let offsetY = 600
   return {
     y: -offsetY + k * (offsetY - 120),
     scale: 1.0 * k * (1 - index * 0.1),
@@ -35,8 +65,8 @@ const bgList = [
 
 export default function SectionBgStack() {
   return (
-    <div className="relative h-80 w-full overflow-hidden px-6 lg:px-12">
-      <div className="relative h-full w-full flex flex-col">
+    <div className="relative h-72 w-full overflow-hidden px-6 lg:px-12 flex flex-col items-center">
+      <div className="relative h-full w-full max-w-2xl">
         <div className="absolute bottom-0 translate-y-full w-full">
           {bgList.map((item, index) => (
             <motion.div
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,27 @@ import {ThemeToggle} from "@/components/ThemeToggle";
 import {UvcanvasLogoFull} from "@/components/Logos";
 import {Button} from "@/components/ui/button";
 import {HeaderPadding} from "@/components/Header";
-import SectionBgStack from "@/app/SectionBgStack";
+import SectionBgStack, {Glow} from "@/app/SectionBgStack";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <HeaderPadding />
-      <div className="flex flex-col items-center px-12 pt-24 pb-12 gap-6">
-        <UvcanvasLogoFull className="h-20"/>
-        <h1 className="hidden">UVCanvas</h1>
-        <p className="text-lg opacity-70 leading-8 max-w-[300px] w-full text-center break-words">
-          A React.js component library for Beautiful shaders canvas
-        </p>
-        <div className="flex gap-4">
-          <Button>Getting Started</Button>
-          <Button variant="secondary">GitHub</Button>
+      <div className="relative w-full">
+        <HeaderPadding />
+        <div className="flex flex-col items-center px-12 pt-24 _pb-12 gap-6">
+          <UvcanvasLogoFull className="h-16"/>
+          <h1 className="hidden">UVCanvas</h1>
+          <p className="text-lg opacity-70 leading-7 max-w-[300px] w-full text-center break-words">
+            A React.js component library for Beautiful shaders canvas
+          </p>
+          <div className="flex gap-4">
+            <Button>Getting Started</Button>
+            <Button variant="secondary">GitHub</Button>
+          </div>
         </div>
+        <SectionBgStack/>
+        <Glow/>
       </div>
-      <SectionBgStack />
     </main>
   );
 }
