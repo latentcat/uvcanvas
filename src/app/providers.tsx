e15d6d75fec20821247d25ed6290bb9fd6b1debc
ciaochaos <1272777550@qq.com>
2024-03-01 21:01:27 +0800

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
+import MixpanelAnalytics from "@/components/MixpanelAnalytics";
 
 export function Providers({
   children
@@ -16,6 +17,7 @@ export function Providers({
       enableSystem
       disableTransitionOnChange
     >
+      <MixpanelAnalytics />
       {children}
     </NextThemesProvider>
   )
