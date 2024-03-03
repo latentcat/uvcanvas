a77dfc8a6656aef97f933cde564718af117f26a0
ciaochaos <1272777550@qq.com>
2024-03-04 01:15:57 +0800

    update

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type {Metadata, Viewport} from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {Providers} from "@/app/providers";
@@ -17,15 +17,36 @@ export const metadata: Metadata = {
     'UVCanvas',
 };
 
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  height: 'device-height',
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false,
+  viewportFit: "cover",
+
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+}
+
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+    <head>
+      <link rel="manifest" href="/manifest.json"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <meta content="yes" name="apple-mobile-web-app-capable"/>
+    </head>
     <body className={inter.className}>
-
     <Providers>
       <Layout>{children}</Layout>
     </Providers>
