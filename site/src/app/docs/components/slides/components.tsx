e1f95524f18860e191d3d1756bedfa7a82c5c87f
倪豪 <1272777550@qq.com>
2024-03-07 22:15:49 +0800

    Update components.tsx

--- site/src/app/docs/components/slides/components.tsx
+++ site/src/app/docs/components/slides/components.tsx
@@ -9,21 +9,25 @@ interface TestAnimationProps {
 
 export function TestAnimation(props: TestAnimationProps) {
   return (
-    <motion.div
-      initial={{
-        x: `${props.step * 10}em`,
-        scale: 0.7,
-      }}
-      animate={{
-        x: `${props.step * 10}em`,
-        scale: 1,
-      }}
-      transition={{ type: 'spring', damping: 10, mass: 0.5, stiffness: 300 }}
-      className="w-[5em] h-[5em] my-[1.5em] rounded-full bg-white text-black flex flex-col items-center justify-center"
+    <div
+      className="w-full flex justify-center"
     >
+      <motion.div
+        initial={{
+          x: `${(props.step - 1) * 10}em`,
+          scale: 0.7,
+        }}
+        animate={{
+          x: `${(props.step - 1) * 10}em`,
+          scale: 1,
+        }}
+        transition={{type: 'spring', damping: 10, mass: 0.5, stiffness: 300}}
+        className="w-[5em] h-[5em] my-[1.5em] rounded-full bg-white text-black flex flex-col items-center justify-center"
+      >
       <span className="text-[2em]">
         {props.step + 1}
       </span>
-    </motion.div>
+      </motion.div>
+    </div>
   )
 }
