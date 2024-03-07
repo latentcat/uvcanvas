3507db61fe61e6ed9b1edb12e7666a2451532d58
倪豪 <1272777550@qq.com>
2024-03-07 20:37:35 +0800

    update dir

--- core/lib/components/slides/components/index.tsx
+++ core/lib/components/slides/components/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import styles from "../styles.module.css";
+import styles from "./styles.module.less";
+
 
 export const defaultComponents = {
   h1: ({ style, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
