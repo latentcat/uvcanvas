6304982d869f23ac5cfc6acab559ba7f24b67e5d
ciaochaos <1272777550@qq.com>
2024-03-08 13:02:36 +0800

    Update slides.tsx

--- site/src/app/docs/components/slides/slides.tsx
+++ site/src/app/docs/components/slides/slides.tsx
@@ -17,7 +17,7 @@ export default function SlidesExample() {
 
   return (
     <div
-      className="rounded-lg overflow-hidden not-prose text-white"
+      className="relative rounded-lg overflow-hidden not-prose text-white"
     >
       <Slides
         mdx={mdxContents}
